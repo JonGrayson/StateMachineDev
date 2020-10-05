3aa37c90ff74946f26e84c0b8d8bd8fbfb15cbf7
JonGrayson <49501559+JonGrayson@users.noreply.github.com>
2020-10-04 20:55:14 -0700

    Add files via upload

---
+++ Scripts/StandingState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandingState : IPlayerState
+{
+    public void Enter(Player player)
+    {
+        player.m_CurrentState = new StandingState();
+        Debug.Log("Standing: Player is standing");
+    }
+
+    public void Execute(Player player)
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            JumpingState jump = new JumpingState();
+            jump.Enter(player);
+        }
+
+        if(Input.GetKeyDown("s"))
+        {
+            DuckingState duck = new DuckingState();
+            duck.Enter(player);
+        }
+    }
+}
